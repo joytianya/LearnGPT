cc992493a935acb589c860b5e04616a7ea7cadd9
matrix <zhangxw0413@gmail.com>
2024-01-06 13:46:34 +0800

    增加model的一些代码

---
+++ llama/model.py
@@ -0,0 +1,40 @@
+
+import torch
+import torch.nn as nn
+
+class LLaMA(nn.Module):
+    def __init__(self, config: LLaMAConfig) -> None:
+        super().__init__()
+        assert config.padded_vocab_size is not None
+        self.config = config
+
+        self.lm_head = nn.Linear(config.n_embd, config.padded_vocab_size, bias=False)
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte = nn.Embedding(config.padded_vocab_size, config.n_embd),
+                h = nn.ModuleList(Block(config) for _ in range(config.n_layer)),
+                ln_f = RMSNorm(config.n_embd),
+            )
+        )
+
+
+class Block(nn.Module):
+    def __init__(self, config: LLaMAConfig) -> None:
+        super().__init__()
+        self.rms_1 = RMSNorm(config.n_embd)
+        self.
+
+class RMSNorm(nn.Module):
+    def __init__(self, size: int, dim: int = -1, eps: float = 1e-5) -> None:
+        super().__init__()
+        self.scale = nn.Parameter(torch.ones(size))
+        self.eps = eps
+        self.dim = dim
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        norm_x = torch.mean(x * x, dim=self.dim, keepdim=True)
+        x_normed = x * torch.rsqrt(norm_x + self.eps)
+
+        return self.scale * x_normed
+
+    
