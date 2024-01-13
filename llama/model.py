3d04a3065ce340a47fde1b614604fc159884cb14
matrix <zhangxw0413@gmail.com>
2024-01-13 11:26:12 +0800

    增加初始化weight

--- llama/model.py
+++ llama/model.py
@@ -1,7 +1,36 @@
 
+from dataclasses import dataclass
+from typing_extensions import Self
+from typing import Optional
+import math
+
 import torch
 import torch.nn as nn
 
+@dataclass
+class LLaMAConfig:
+    block_size: int = 2048
+    vocab_size: int = 32000
+    padded_vocab_size: Optional[int] = None
+    n_layer: int = 32
+    n_head: int = 32
+    n_embd: int = 4096
+
+    def __post_init__(self):
+        if self.padded_vocab_size is None:
+            self.padded_vocab_size = find_multiple(self.vocab_size, 64)
+
+    @classmethod
+    def from_name(cls, name: str) -> Self:
+        return cls(**llama_configs[name])
+
+
+llama_configs = {
+    "7B": dict(n_layer=32, n_head=32, n_embd=4096),
+    "13B": dict(n_layer=40, n_head=40, n_embd=5120),
+    "30B": dict(n_layer=60, n_head=52, n_embd=6656),
+    "65B": dict(n_layer=80, n_head=64, n_embd=8192),
+}
 
 
 class LLaMA(nn.Module):
@@ -13,12 +42,23 @@ class LLaMA(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.padded_vocab_size, bias=False)
         self.transformer = nn.ModuleDict(
             dict(
-                wte = nn.Embedding(config.padded_vocab_size, config.n_embd),
-                h = nn.ModuleList(Block(config) for _ in range(config.n_layer)),
-                ln_f = RMSNorm(config.n_embd),
+                wte=nn.Embedding(config.padded_vocab_size, config.n_embd),
+                h=nn.ModuleList(Block(config) for _ in range(config.n_layer)),
+                ln_f=RMSNorm(config.n_embd),
             )
         )
 
+        self.rope_cache: Optional[RoPECache] = None
+        self.mask_cache: Optional[MaskCache] = None
+        self.kv_cache: Optional[KVCache] = None
+    
+    # 定义每部分初始化权重
+    def _init_weights(self, module: nn.Module) -> None:
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02 / math.sqrt(2 * self.config.n_layer))
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02 / math.sqrt(2 * self.config.n_layer))
+            
 
 class Block(nn.Module):
     def __init__(self, config: LLaMAConfig) -> None:
